fb7c723aaa00112a77fb7cfb715188b7bc45ca9f
sofa_king <apalm112@gmail.com>
2018-04-18 14:56:48 -0700

    Added container component per rubric and comments to DataFetch.js

---
+++ src/components/DataContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DataContainer = (props) => {
+
+		// TODO: You should do all data fetching from a container component that passes the data down to presentational components, via props, responsible for displaying images!
+		// ** A container component that takes in a keyword and api key as props, and fetches the photos and other required information from the API
+		// // const results = props.passFlickrPhotos;
+		//			<h2>{props.searchText}</h2>
+		//			onClick={props.thingToRenameSoFunctionGetsPassed} >
+
+
+	return (
+
+	);
+};
+
+export default DataContainer;
--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -3,6 +3,13 @@ import React from 'react';
 const DataFetch = (props) => {
 
 		// TODO: You should do all data fetching from a container component that passes the data down to presentational components responsible for displaying images!
+		// ** A container component that takes in a keyword and api key as props, and fetches the photos and other required information from the API
+		// const results = props.passFlickrPhotos;
+		//			<h2>{props.searchText}</h2>
+		//			onClick={props.thingToRenameSoFunctionGetsPassed} >
+		//			Data fetched from a "container" component that passes data down to presentation component via props
+
+		
 		// Default query value for initial page load.
 		performSearch = (query='tomato') => {
 			axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPI}&tags=${query}&per_page=12&page=1&format=json&nojsoncallback=1`)
