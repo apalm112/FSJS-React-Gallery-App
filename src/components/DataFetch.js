b7993d665f9230d2b0d00657357058920134cb5f
sofa_king <apalm112@gmail.com>
2018-04-18 21:04:08 -0700

    Started building a data fetching component, might be able to modify y MainNav component to do the data fetching.

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -1,39 +1,38 @@
 import React from 'react';
 
-const DataFetch = (props) => {
+import axios from 'axios';
 
-		// TODO: You should do all data fetching from a container component that passes the data down to presentational components responsible for displaying images!
-		// ** A container component that takes in a keyword and api key as props, and fetches the photos and other required information from the API
-		// const results = props.passFlickrPhotos;
-		//			<h2>{props.searchText}</h2>
-		//			onClick={props.thingToRenameSoFunctionGetsPassed} >
-		//			Data fetched from a "container" component that passes data down to presentation component via props
+const DataFetch = (props, flickrAPI) => {
 
-		
-		// Default query value for initial page load.
-		performSearch = (query='tomato') => {
-			axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPI}&tags=${query}&per_page=12&page=1&format=json&nojsoncallback=1`)
+	// TODO: You should do all data fetching from a container component that passes the data down to presentational components responsible for displaying images!
+	// ** A container component that takes in a keyword and api key as props, and fetches the photos and other required information from the API
+	// const results = props.passFlickrPhotos;
+	//			<h2>{props.searchText}</h2>
+	//			onClick={props.thingToRenameSoFunctionGetsPassed} >
+	//			Data fetched from a "container" component that passes data down to presentation component via props
+
+
+	// Default query value for initial page load.
+	const performSearch = (query='tomato', flickrAPI) => {
+		axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPI}&tags=${query}&per_page=12&page=1&format=json&nojsoncallback=1`)
 			.then(response => {
 				let resFlickrData = response.data.photos.photo;
-					this.setState({
+				this.setState({
 					flickrPhotos: resFlickrData.map((photo) => {
 						return {
 							...photo,
-						}
+						};
 					}),
 					loading: false,
 					searchText: query,
-				})
+				});
 				// console.log(this.state.flickrPhotos);
 				console.log('searchText Value is: ', this.state.searchText);
 			})
 			.catch(error => {
-				console.error("Error fetching & parsing the data.", error);
-			})
-		}
-	// return (
-	//
-	// );
+				console.error('Error fetching & parsing the data.', error);
+			});
+	};
 };
 
 export default DataFetch;
