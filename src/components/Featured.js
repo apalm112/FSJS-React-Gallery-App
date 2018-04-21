a92626783cfe9dfd77b98facece6618967ae9ef4
sofa_king <apalm112@gmail.com>
2018-04-20 19:25:37 -0700

    Removed commented out code, added coTODO comments, BUG comment.

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -6,11 +6,15 @@ import { NavLink } from 'react-router-dom';
 const Featured = (props) => {
 	let item = props.title;
 	return (
+		// BUG: 
+		// Active class styling works w/ <Link>, does Not work w/ <NavLink>
+		// But using <Link> causes the browser to infinite loop run performSearch() so
+		// the app freezes up.
 		// <li><Link to={props.title} onClick={props.performSearch(item)} >{props.title}</Link></li>
 		<li><NavLink
-			activeClassName="active"
 			to={`${item}`}
-			activeStyle={{ backgroundColor: '#275270' }}
+			exact activeClassName="active"
+			// activeStyle={{ backgroundColor: '#275270' }}
 			onClick={() => props.performSearch(`${item}`)} >
 			{item}
 		</NavLink></li>
