66ba005b465bf975df619d563ab3a5c888ad016d
Adam <apalm112@gmail.com>
2018-06-18 17:05:56 -0400

    Changed file names.

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
-import flickrAPI from '../.myConfig';
+import flickrAPI from '../config';
 import PhotoContainer from './PhotoContainer';
 import Loading from './exceeds/Loading';
 
