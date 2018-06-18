f2fa96871782542860e87fa2d9cc47ec8d77c78b
Adam <apalm112@gmail.com>
2018-06-18 17:09:41 -0400

    Fixed paran to bracket.

--- src/components/exceeds/FourZeroFour.js
+++ src/components/exceeds/FourZeroFour.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const fourZeroFour = () => (
+const fourZeroFour = () => {
 	<li className='not-found'>
 		<i className="material-icons icn-error">error</i>
 		<h3>404, Eh.</h3>
 		<p>Page Not Found, please try again.</p>
 	</li>
-);
+};
 
 export default fourZeroFour;
