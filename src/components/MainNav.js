3942b406847392680e67c7cc3bd0414d02def71a
Adam <apalm112@gmail.com>
2019-02-21 19:40:59 -0500

    Removed the SearchForm component from the MainNav.

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
-
-import SearchForm from './SearchForm';
+import { NavLink } from 'react-router-dom';
 
 const MainNav = (props) => {
 	// MainNav is a stateless component written using an arrow function, when describing a component using a function you can access props by passing the function the props object via the props argument.
 	return (
 		<header>
-			{/* This Route does not render the SearchForm component when a MainNav link is clicked.  */}
-			<Route path="/search" render={ () => <SearchForm props={props} /> } />
 			<nav className="main-nav">
 				<ul>
 					<li><NavLink
