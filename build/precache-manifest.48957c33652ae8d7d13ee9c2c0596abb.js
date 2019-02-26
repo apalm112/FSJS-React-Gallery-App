ea3413c8ef99715e23d4de522b62c7ad376928d9
Adam <apalm112@gmail.com>
2019-02-26 16:53:43 -0500

    Updates for Herkou deployment & ran 'npm run build'.

---
+++ build/precache-manifest.48957c33652ae8d7d13ee9c2c0596abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5974169875c02cb54522",
+    "url": "/static/js/main.59741698.chunk.js"
+  },
+  {
+    "revision": "d0d06e070565b5a23751",
+    "url": "/static/js/2.d0d06e07.chunk.js"
+  },
+  {
+    "revision": "5974169875c02cb54522",
+    "url": "/static/css/main.087c9392.chunk.css"
+  },
+  {
+    "revision": "a5c07daafc14040ad85caadb703edd37",
+    "url": "/index.html"
+  }
+];
