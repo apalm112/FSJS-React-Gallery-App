64e997f649b548fb9d9eda110dcd5fc9ada36e65
Adam <apalm112@gmail.com>
2019-07-10 10:28:15 -0400

    Modified the res.sendFile() method.

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 });*/
 
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	// res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile('index.html', { root: path.join(__dirname, '../../../dist') });
 });
 
 app.listen(port);
